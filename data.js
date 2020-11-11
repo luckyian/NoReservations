96c3e9fe19f493eea69afd59e9f9064e6f8080ba
Kenneth Kopelson <kennethkopelson@gmail.com>
2020-11-10 19:25:43 -0800

    data.js

---
+++ data.js
@@ -0,0 +1,45 @@
+// Table (DATA)
+// =============================================================
+const data = [
+    {
+        Name: "",
+        Phone: "",
+        Email: "",
+        UniqueID: 1
+    },
+    {
+        Name: "",
+        Phone: "",
+        Email: "",
+        UniqueID: 2
+    },
+    {
+        Name: "",
+        Phone: "",
+        Email: "",
+        UniqueID: 3
+    },
+    {
+        Name: "",
+        Phone: "",
+        Email: "",
+        UniqueID: 4
+    },
+    {
+        Name: "",
+        Phone: "",
+        Email: "",
+        UniqueID: 5
+    }
+]
+
+const waitList = [
+    {
+        Name: "test",
+        Phone: "test",
+        Email: "test",
+        UniqueID: 6
+    }
+]
+
+module.exports = data, waitList;
